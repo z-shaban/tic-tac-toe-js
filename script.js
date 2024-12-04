b71e9451b2ffdad5aeed8d53985e67d5de8e1eee
shaban <146794146+sarzhh@users.noreply.github.com>
2024-12-04 14:13:36 +0000

    Add win logic

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function Gameboard (){
 
     const printBoard = () => {
         const boardWithValues = board.map((row) => row.map((cell) => cell.getValue()));
-        console.log(boardWithValues);
+        return boardWithValues;
     }
  
     
@@ -73,16 +73,46 @@ function Players(playerOneName = "Player 1", playerTwoName = "Player 2"){
 function Game(){
     const board = Gameboard();
     const players = Players();
+    
 
     const playRound = (row, column,) =>{
         board.selectCell(row,column, players.getActivePlayer().symbol);
-
+        winRound();
         players.switchPlayerTurn();
         board.printBoard();
+        
     }
 
-    const winRound = () =>{
+    const winRound = () =>{ 
+         const myBoard = board.printBoard();
+         const myPlayer = players.getActivePlayer();
+         console.log(myBoard); 
 
+         //check for horizontal win
+         for (let i = 0; i < myBoard.length; i++){
+            for (let j = 0; j < myBoard[i].length; j++){
+                if(myPlayer.symbol === myBoard[i][j] && myBoard[i][j] === myBoard[i][j+1] && myBoard[i][j+1] === myBoard[i][j+2]){
+                    console.log("iam a horizontal win");
+                } 
+            }
+        }
+        //check for vertical win
+        for (let i = 0; i < myBoard.length-2; i++){
+            for (let j = 0; j < myBoard[i].length; j++){
+                if(myPlayer.symbol === myBoard[i][j] && myBoard[i][j] == myBoard[i+1][j] && myBoard[i+1][j] == myBoard[i+2][j]){
+                    console.log("i am a vertical win");
+                }
+            }
+        }
+
+        //check for diagonal win
+        
+                if(myPlayer.symbol === myBoard[0][0] && myBoard[0][0] == myBoard[1][1] && myBoard[1][1] == myBoard[2][2]||
+                   myPlayer.symbol === myBoard[0][2] && myBoard[0][2] == myBoard[1][1] && myBoard[1][1] == myBoard[2][0]
+                ){
+                    console.log("i am a diagonal win");
+                }
+            
     }
 
     return{playRound}
@@ -90,6 +120,8 @@ function Game(){
 
 const game = Game();
 game.playRound(0, 1);
+game.playRound(0, 2);
+game.playRound(0, 0);
 game.playRound(1, 1);
-game.playRound(0,2)
-game.playRound(2,2)
+game.playRound(0, 0);
+game.playRound(2, 0);
