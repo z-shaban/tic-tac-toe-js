f835d97132da03ad4d19867b56528d94321af2d3
shaban <146794146+sarzhh@users.noreply.github.com>
2024-12-05 21:46:47 +0000

    Add html component to view in browser

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function Gameboard (){
     const rows = 3;
     const columns = 3;
@@ -87,12 +89,14 @@ function Game(){
          const myBoard = board.printBoard();
          const myPlayer = players.getActivePlayer();
          console.log(myBoard); 
+         const winMessage = document.querySelector(".winMessage");
+         winMessage.textContent = ""
 
          //check for horizontal win
          for (let i = 0; i < myBoard.length; i++){
             for (let j = 0; j < myBoard[i].length; j++){
                 if(myPlayer.symbol === myBoard[i][j] && myBoard[i][j] === myBoard[i][j+1] && myBoard[i][j+1] === myBoard[i][j+2]){
-                    console.log("iam a horizontal win");
+                    winMessage.textContent = `${myPlayer.name} wins!!!...` 
                 } 
             }
         }
@@ -100,28 +104,74 @@ function Game(){
         for (let i = 0; i < myBoard.length-2; i++){
             for (let j = 0; j < myBoard[i].length; j++){
                 if(myPlayer.symbol === myBoard[i][j] && myBoard[i][j] == myBoard[i+1][j] && myBoard[i+1][j] == myBoard[i+2][j]){
-                    console.log("i am a vertical win");
+                    winMessage.textContent = `${myPlayer.name} wins!!!...` 
                 }
             }
         }
 
         //check for diagonal win
-        
-                if(myPlayer.symbol === myBoard[0][0] && myBoard[0][0] == myBoard[1][1] && myBoard[1][1] == myBoard[2][2]||
-                   myPlayer.symbol === myBoard[0][2] && myBoard[0][2] == myBoard[1][1] && myBoard[1][1] == myBoard[2][0]
-                ){
-                    console.log("i am a diagonal win");
-                }
+        if(myPlayer.symbol === myBoard[0][0] && myBoard[0][0] == myBoard[1][1] && myBoard[1][1] == myBoard[2][2]||
+            myPlayer.symbol === myBoard[0][2] && myBoard[0][2] == myBoard[1][1] && myBoard[1][1] == myBoard[2][0]){
+                winMessage.textContent = `${myPlayer.name} wins!!!...` 
+        }
             
     }
 
-    return{playRound}
+    return{
+        playRound,
+        getBoard : board.getBoard,
+        getActivePlayer : players.getActivePlayer
+    }
 }
 
-const game = Game();
-game.playRound(0, 1);
-game.playRound(0, 2);
-game.playRound(0, 0);
-game.playRound(1, 1);
-game.playRound(0, 0);
-game.playRound(2, 0);
+function screenController (){
+    const game = Game();
+    const playerTurnDiv = document.querySelector(".turn");
+    const boardDiv = document.querySelector(".board");
+
+    const updateScreen = () =>{
+        boardDiv.textContent ="";
+
+        const board = game.getBoard();
+        const activePlayer = game.getActivePlayer();
+
+        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+        board.forEach((row, rowIndex) =>{
+            const rowDiv = document.createElement("div");
+            rowDiv.classList.add("row");
+            row.forEach((cell,columnIndex) =>{
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                cellButton.textContent = cell.getValue();
+
+                cellButton.dataset.row = rowIndex; 
+                cellButton.dataset.column = columnIndex; 
+
+                rowDiv.appendChild(cellButton);
+                
+
+            })
+            boardDiv.appendChild(rowDiv);
+        })
+    }
+
+    function clickHandler(e){
+        const selectedRow = e.target.dataset.row;
+        const selectedColumn = e.target.dataset.column;
+
+        if(!selectedColumn && selectedRow) return;
+        game.playRound(selectedRow, selectedColumn)
+        updateScreen();
+        
+    }
+
+    boardDiv.addEventListener("click", clickHandler);
+
+    updateScreen();
+}
+
+screenController();
+
+
+
